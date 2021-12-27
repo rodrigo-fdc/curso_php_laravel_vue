989e4c9fa820b1a4580d68a472ca65970834eaf5
Rodrigo Ferreira <rodrigoferreira.mkt@gmail.com>
2021-12-27 18:08:28 -0300

    Enviando primeiras rotas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Olá seja bem vindo!';
+});
+
+Route::get('/sobre-nos', function () {
+    return 'Sobre Nós';
+});
+
+
+Route::get('/contato', function () {
+    return 'Contato';
 });
